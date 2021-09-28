0f14a5b085f14c906d001ee0c045b82e09ac1468
Aidar Zhusipov <35493222+High-res@users.noreply.github.com>
2021-09-28 13:06:20 +0600

    Add files via upload

--- actions/__init__.py
+++ actions/__init__.py
@@ -3,4 +3,4 @@ from .getWorkers import getWorkerss, addWorkers, updateWorker, removeWorkers
 from .getInfo import getLocation, getAvans, getZhaloba
 from .msgInfo import getMessages, deleteMessages, addMesages
 from .addChallenge import addChallenge, getChallengeAction, getChallenge, removeChallenge, updateChalenges
-from .addExcel import toXlsx
+from .addExcel import toXlsxThisMonth, toXlsxLastMonth
--- actions/addExcel.py
+++ actions/addExcel.py
@@ -1,15 +1,22 @@
 import pandas as pd
 import numpy as np
-from datetime import date
+from datetime import date, timedelta
 import actions
 
 
-def allDates() :
+def thisMonth() :
     today = date.today()
     day = today.strftime('%Y-%m')
     return day
 
-def toXlsx() :
+def lastMonth() :
+    today = date.today()
+    first = today.replace(day=1)
+    lastMonth = first - timedelta(days=1)
+    day = lastMonth.strftime("%Y-%m")
+    return day
+
+def toXlsxThisMonth() :
     WorkerName = []
     WorkerInJob = []
     Worker2Gis = []
@@ -18,7 +25,7 @@ def toXlsx() :
     locations = actions.getLocation()
     for worker in workers :
         for location in locations :
-            if allDates() == location['date_today'] :
+            if thisMonth() == location['date_today'] :
                 if location['user_tg_id'] == worker['tg_id'] :
                     WorkerName.append(worker['name'])
                     if str(location['location_longitude']) > str(76.900000) and str(location['location_longitude']) < str(76.913000) and str(location['location_latitude']) < str(43.286000) and str(location['location_latitude']) > str(43.284000) :
@@ -36,4 +43,32 @@ def toXlsx() :
     dataset['Дата/время получения получения'] = workersss[3].tolist()
     print('Вывели xlsx')
     dataset.to_excel('./static/teams.xlsx', index=False)
+
+def toXlsxLastMonth() :
+    WorkerName = []
+    WorkerInJob = []
+    Worker2Gis = []
+    WorkerSendDate = []
+    workers = actions.getWorkerss()
+    locations = actions.getLocation()
+    for worker in workers :
+        for location in locations :
+            if lastMonth() == location['date_today'] :
+                if location['user_tg_id'] == worker['tg_id'] :
+                    WorkerName.append(worker['name'])
+                    if str(location['location_longitude']) > str(76.900000) and str(location['location_longitude']) < str(76.913000) and str(location['location_latitude']) < str(43.286000) and str(location['location_latitude']) > str(43.284000) :
+                        WorkerInJob.append('Да')
+                    else :
+                        WorkerInJob.append('Нет')
+                    Worker2Gis.append('https://2gis.kz/almaty?m='+location['location_longitude']+ '%2C'+location['location_latitude']+'%2F19.35')
+                    WorkerSendDate.append(location['send_date'])
+    workersss = np.array([WorkerName, WorkerInJob, Worker2Gis, WorkerSendDate])
+
+    dataset = pd.DataFrame()
+    dataset['Имя работника'] = workersss[0].tolist()
+    dataset['Возле работы? Да/Нет'] = workersss[1].tolist()
+    dataset['Ссылка на карту'] = workersss[2].tolist()
+    dataset['Дата/время получения получения'] = workersss[3].tolist()
+    print('Вывели xlsx')
+    dataset.to_excel('./static/last.xlsx', index=False)
 
